95fee5f90f31d7eb8bf9b5cc91b416cd673b7bc5
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-28 19:18:34 -0400

    Setup Navigation Components

---
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/favorites/FavoriteRecipesFragment.kt
@@ -0,0 +1,8 @@
+package com.gmail.orlandroyd.foody.presentation.favorites
+
+import androidx.fragment.app.Fragment
+import com.gmail.orlandroyd.foody.R
+
+class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
+
+}
---
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/foodjoke/FoodJokeFragment.kt
@@ -0,0 +1,8 @@
+package com.gmail.orlandroyd.foody.presentation.foodjoke
+
+import androidx.fragment.app.Fragment
+import com.gmail.orlandroyd.foody.R
+
+class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
+
+}
