bf24c025673edf3578cba8aecadbc2bda273f0db
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 18:36:28 -0400

    Display Ingredients Data

--- app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/ingredients/IngredientsFragment.kt
@@ -1,8 +1,45 @@
 package com.gmail.orlandroyd.foody.presentation.details.ingredients
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.gmail.orlandroyd.foody.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gmail.orlandroyd.foody.data.remote.dto.ResultDto
+import com.gmail.orlandroyd.foody.databinding.FragmentIngredientsBinding
+import com.gmail.orlandroyd.foody.util.Constants.Companion.RECIPE_RESULT_KEY
 
-class IngredientsFragment : Fragment(R.layout.fragment_ingredients) {
+class IngredientsFragment : Fragment() {
 
+    private val mAdapter: IngredientsAdapter by lazy { IngredientsAdapter() }
+
+    private var _binding: FragmentIngredientsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: ResultDto? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        setupRecyclerView()
+        myBundle?.extendedIngredients?.let { mAdapter.setData(it) }
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.ingredientsRecyclerview.adapter = mAdapter
+        binding.ingredientsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
