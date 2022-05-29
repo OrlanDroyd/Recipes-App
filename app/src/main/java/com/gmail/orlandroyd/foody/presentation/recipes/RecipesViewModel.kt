454c1ffff2bceea6ebeaf4abc02012de3c3f37ee
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 15:04:19 -0400

    RecipesViewModel.kt datastore injected

--- app/src/main/java/com/gmail/orlandroyd/foody/presentation/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/recipes/RecipesViewModel.kt
@@ -2,6 +2,9 @@ package com.gmail.orlandroyd.foody.presentation.recipes
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.gmail.orlandroyd.foody.data.datastore.DataStoreRepository
+import com.gmail.orlandroyd.foody.data.datastore.MealAndDietType
 import com.gmail.orlandroyd.foody.util.Constants
 import com.gmail.orlandroyd.foody.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.gmail.orlandroyd.foody.util.Constants.Companion.QUERY_API_KEY
@@ -10,12 +13,31 @@ import com.gmail.orlandroyd.foody.util.Constants.Companion.QUERY_FILL_INGREDIENT
 import com.gmail.orlandroyd.foody.util.Constants.Companion.QUERY_NUMBER
 import com.gmail.orlandroyd.foody.util.Constants.Companion.QUERY_TYPE
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipesViewModel @Inject constructor(
     application: Application,
+    private val dataStoreRepository: DataStoreRepository
 ) : AndroidViewModel(application) {
+
+    private lateinit var mealAndDiet: MealAndDietType
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType() =
+        viewModelScope.launch(Dispatchers.IO) {
+            if (this@RecipesViewModel::mealAndDiet.isInitialized) {
+                dataStoreRepository.saveMealAndDietType(
+                    mealAndDiet.selectedMealType,
+                    mealAndDiet.selectedMealTypeId,
+                    mealAndDiet.selectedDietType,
+                    mealAndDiet.selectedDietTypeId
+                )
+            }
+        }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
