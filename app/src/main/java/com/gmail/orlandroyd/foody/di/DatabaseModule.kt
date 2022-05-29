e4aa0463ed88cb0561fe89ea739b6c74cf70ca9a
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 12:38:10 -0400

    DatabaseModule class created (DI)

---
+++ app/src/main/java/com/gmail/orlandroyd/foody/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.gmail.orlandroyd.foody.di
+
+import android.content.Context
+import androidx.room.Room
+import com.gmail.orlandroyd.foody.data.local.RecipesDatabase
+import com.gmail.orlandroyd.foody.util.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+}
