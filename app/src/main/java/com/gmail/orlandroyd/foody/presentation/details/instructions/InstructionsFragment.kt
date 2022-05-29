df0c4df7fe34607f8369db3771802d745a7cd623
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 18:43:37 -0400

    InstructionsFragment.kt WebView added

--- app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/instructions/InstructionsFragment.kt
@@ -1,8 +1,40 @@
 package com.gmail.orlandroyd.foody.presentation.details.instructions
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.gmail.orlandroyd.foody.R
+import com.gmail.orlandroyd.foody.data.remote.dto.ResultDto
+import com.gmail.orlandroyd.foody.databinding.FragmentInstructionsBinding
+import com.gmail.orlandroyd.foody.util.Constants.Companion.RECIPE_RESULT_KEY
 
-class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
+class InstructionsFragment : Fragment() {
 
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: ResultDto? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
