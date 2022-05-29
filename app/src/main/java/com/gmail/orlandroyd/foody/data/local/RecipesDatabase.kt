a07c608c833ebeaf541a6f4b4709870b529f3011
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 19:01:58 -0400

    RecipesDatabase.kt modified

--- app/src/main/java/com/gmail/orlandroyd/foody/data/local/RecipesDatabase.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/data/local/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.gmail.orlandroyd.foody.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.gmail.orlandroyd.foody.data.local.entities.FavoritesEntity
 import com.gmail.orlandroyd.foody.data.local.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
