1ec5e1e23abd9abd4b44a4cf429e76316dd019ca
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 12:41:14 -0400

    LocalDataSource class created

--- app/src/main/java/com/gmail/orlandroyd/foody/data/Repository.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/data/Repository.kt
@@ -1,12 +1,15 @@
 package com.gmail.orlandroyd.foody.data
 
+import com.gmail.orlandroyd.foody.data.local.LocalDataSource
 import com.gmail.orlandroyd.foody.data.remote.RemoteDataSource
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
 class Repository @Inject constructor(
-    remoteDataSource: RemoteDataSource
+    remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource
 ) {
     val remote = remoteDataSource
+    val local = localDataSource
 }
---
+++ app/src/main/java/com/gmail/orlandroyd/foody/data/local/LocalDataSource.kt
@@ -0,0 +1,19 @@
+package com.gmail.orlandroyd.foody.data.local
+
+import com.gmail.orlandroyd.foody.data.local.entities.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao
+) {
+
+    fun readRecipes(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+
+}
