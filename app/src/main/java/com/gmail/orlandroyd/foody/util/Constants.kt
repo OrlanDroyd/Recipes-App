a0ee05bbad3c94fdfacf4155f6830e876ff81683
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-28 22:40:49 -0400

    Constants class created

---
+++ app/src/main/java/com/gmail/orlandroyd/foody/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.gmail.orlandroyd.foody.util
+
+import com.gmail.orlandroyd.foody.BuildConfig
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
+        const val API_KEY = BuildConfig.API_KEY
+
+    }
+
+}
