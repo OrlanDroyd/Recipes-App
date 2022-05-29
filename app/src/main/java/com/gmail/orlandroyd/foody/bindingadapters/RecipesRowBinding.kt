be81896d69b4bfc45ec94b139f0ff71aed14a7f7
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 11:04:43 -0400

    RecipesAdapter created

---
+++ app/src/main/java/com/gmail/orlandroyd/foody/bindingadapters/RecipesRowBinding.kt
@@ -0,0 +1,39 @@
+package com.gmail.orlandroyd.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.gmail.orlandroyd.foody.R
+
+class RecipesRowBinding {
+
+    companion object {
+
+        @BindingAdapter("applyVeganColor")
+        @JvmStatic
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
+                        view.setTextColor(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                    is ImageView -> {
+                        view.setColorFilter(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/recipes/RecipesAdapter.kt
@@ -0,0 +1,53 @@
+package com.gmail.orlandroyd.foody.presentation.recipes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.orlandroyd.foody.databinding.RecipesRowLayoutBinding
+import com.gmail.orlandroyd.foody.domain.model.FoodRecipe
+import com.gmail.orlandroyd.foody.domain.model.Result
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipes = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result){
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setData(newData: FoodRecipe){
+        val recipesDiffUtil =
+            RecipesDiffUtil(recipes, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
