1c24f26f4e4bf0c8cc8f3ebfb484d2bd1205920b
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-30 15:09:15 -0400

    FoodJokeBinding and fragment_food_joke.xml modified

--- app/src/main/java/com/gmail/orlandroyd/foody/bindingadapters/FoodJokeBinding.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/bindingadapters/FoodJokeBinding.kt
@@ -56,6 +56,9 @@ class FoodJokeBinding {
                         }
                     }
                 }
+                else -> {
+                    // Nothing
+                }
             }
         }
 
