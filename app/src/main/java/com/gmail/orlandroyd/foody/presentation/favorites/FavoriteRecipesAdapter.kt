831735512de24c842921ae9643123f80c84b0428
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 21:09:27 -0400

    FavoriteRecipesAdapter.kt created

---
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/favorites/FavoriteRecipesAdapter.kt
@@ -0,0 +1,56 @@
+package com.gmail.orlandroyd.foody.presentation.favorites
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.orlandroyd.foody.data.local.entities.FavoritesEntity
+import com.gmail.orlandroyd.foody.databinding.FavoriteRecipesRowLayoutBinding
+import com.gmail.orlandroyd.foody.presentation.recipes.RecipesDiffUtil
+
+class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+
+    private var favoriteRecipes = emptyList<FavoritesEntity>()
+
+    class MyViewHolder(val binding: FavoriteRecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(favoritesEntity: FavoritesEntity) {
+            binding.favoritesEntity = favoritesEntity
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = FavoriteRecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentRecipe = favoriteRecipes[position]
+        holder.bind(currentRecipe)
+
+    }
+
+    override fun getItemCount(): Int {
+        return favoriteRecipes.size
+    }
+
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
+        val favoriteRecipesDiffUtil =
+            RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+}
