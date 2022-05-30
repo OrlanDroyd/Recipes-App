aad7a3ec42659dc6efacc4cc1a2ee9a0b7ebc2ec
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-30 19:16:36 -0400

    Change style and fix Dark Theme bugs

--- app/src/main/java/com/gmail/orlandroyd/foody/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setTheme(R.style.Theme_Foody)
+        setTheme(R.style.AppTheme)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
