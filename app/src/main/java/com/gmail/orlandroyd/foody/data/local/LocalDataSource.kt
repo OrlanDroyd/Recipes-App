74cde43aa89b25e12422889eaf32431fdff7425a
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-30 14:27:17 -0400

    LocalDataSource.kt modified (FoodJoke)

--- app/src/main/java/com/gmail/orlandroyd/foody/data/local/LocalDataSource.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/data/local/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.gmail.orlandroyd.foody.data.local
 
 import com.gmail.orlandroyd.foody.data.local.entities.FavoritesEntity
+import com.gmail.orlandroyd.foody.data.local.entities.FoodJokeEntity
 import com.gmail.orlandroyd.foody.data.local.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -17,12 +18,20 @@ class LocalDataSource @Inject constructor(
         return recipesDao.readFavoriteRecipes()
     }
 
+    fun readFoodJoke(): Flow<List<FoodJokeEntity>> {
+        return recipesDao.readFoodJoke()
+    }
+
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
     }
 
     suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
         recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun insertFoodJoke(foodJokeEntity: FoodJokeEntity) {
+        recipesDao.insertFoodJoke(foodJokeEntity)
     }
 
     suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) {
