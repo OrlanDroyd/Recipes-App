275deb77893fc6fe9861a9826a853c1d315fe130
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-30 18:04:26 -0400

    Splash Screen added

--- app/src/main/java/com/gmail/orlandroyd/foody/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setTheme(R.style.Theme_Foody)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
