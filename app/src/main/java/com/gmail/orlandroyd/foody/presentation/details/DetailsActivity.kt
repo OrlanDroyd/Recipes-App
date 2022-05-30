e0830b1e788450e0242ae862c8261f9e53c0f7c5
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 20:41:25 -0400

    Save recipe to favorite

--- app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/DetailsActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/details/DetailsActivity.kt
@@ -3,16 +3,20 @@ package com.gmail.orlandroyd.foody.presentation.details
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.navArgs
+import com.gmail.orlandroyd.foody.MainViewModel
 import com.gmail.orlandroyd.foody.R
+import com.gmail.orlandroyd.foody.data.local.entities.FavoritesEntity
 import com.gmail.orlandroyd.foody.databinding.ActivityDetailsBinding
 import com.gmail.orlandroyd.foody.presentation.details.ingredients.IngredientsFragment
 import com.gmail.orlandroyd.foody.presentation.details.instructions.InstructionsFragment
 import com.gmail.orlandroyd.foody.presentation.details.overview.OverviewFragment
 import com.gmail.orlandroyd.foody.util.Constants.Companion.RECIPE_RESULT_KEY
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
@@ -20,6 +24,8 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding
 
     private val args by navArgs<DetailsActivityArgs>()
+
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,8 +73,34 @@ class DetailsActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
             finish()
+        } else if (item.itemId == R.id.save_to_favorites_menu) {
+            saveToFavorites(item)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveToFavorites(item: MenuItem) {
+        val favoritesEntity =
+            FavoritesEntity(
+                0,
+                args.result
+            )
+        mainViewModel.insertFavoriteRecipe(favoritesEntity)
+        changeMenuItemColor(item, R.color.yellow)
+        showSnackBar("Recipe saved.")
+    }
+
+    private fun changeMenuItemColor(item: MenuItem, color: Int) {
+        item.icon.setTint(ContextCompat.getColor(this, color))
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(
+            binding.detailsLayout,
+            message,
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay") {}
+            .show()
     }
 
 }
