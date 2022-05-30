f06546f07c7c8d3b4629b0c7e6d055b53866b090
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-29 21:55:42 -0400

    Display Favorites recipes

--- app/src/main/java/com/gmail/orlandroyd/foody/presentation/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/gmail/orlandroyd/foody/presentation/favorites/FavoriteRecipesFragment.kt
@@ -1,8 +1,44 @@
 package com.gmail.orlandroyd.foody.presentation.favorites
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.gmail.orlandroyd.foody.R
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.orlandroyd.foody.MainViewModel
+import com.gmail.orlandroyd.foody.databinding.FragmentFavoriteRecipesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
+@AndroidEntryPoint
+class FavoriteRecipesFragment : Fragment() {
 
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        setupRecyclerView(binding.favoriteRecipesRecyclerView)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner) {
+            mAdapter.setData(it)
+        }
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
