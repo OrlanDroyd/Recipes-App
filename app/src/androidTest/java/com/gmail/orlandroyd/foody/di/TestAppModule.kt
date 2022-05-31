939bd138d7350ae3a05ea95ecca2d87b3eadb8f5
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-31 18:28:03 -0400

    TestAppModule.kt created

---
+++ app/src/androidTest/java/com/gmail/orlandroyd/foody/di/TestAppModule.kt
@@ -0,0 +1,25 @@
+package com.gmail.orlandroyd.foody.di
+
+import android.app.Application
+import androidx.room.Room
+import com.gmail.orlandroyd.foody.data.local.RecipesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideRecipesDatabase(app: Application): RecipesDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            RecipesDatabase::class.java
+        ).build()
+    }
+
+}
