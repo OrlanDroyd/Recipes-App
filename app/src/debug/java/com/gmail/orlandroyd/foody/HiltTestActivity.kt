46e94ddd705ac5327578c17aea9feb6ba88313fd
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-31 19:10:51 -0400

    HiltTestActivity.kt and HiltExt.kt created

---
+++ app/src/debug/java/com/gmail/orlandroyd/foody/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.gmail.orlandroyd.foody
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
